0614005bb5b25fd39aa6d0e096518e38ddd5a188
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-01-14 15:37:33 +0000

    edited some meta links

--- src/core/config.ts
+++ src/core/config.ts
@@ -54,9 +54,8 @@ export const SOCIAL_MEDIA = [
 export const META_DEFAULTS = {
   custom: [],
   description:
-    "Open-source PWA storefront built with Saleor's e-commerce GraphQL API. Written with React and TypeScript.",
-  image: `${window.location.origin}${require("../images/logo.svg")}`,
-  title: "Demo PWA Storefront – Saleor Commerce",
+    "Ruth Packham: Artist/Maker",
+  title: "Ruth Packham",
   type: "website",
   url: window.location.origin,
 };
