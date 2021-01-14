d7777db4bd56d1a6d37c9aafdcd4b49df287bb72
Luka Abeywickrama <luka.abeywickrama@hotmail.co.uk>
2021-01-14 15:27:07 +0000

    update links to socials

--- src/core/config.ts
+++ src/core/config.ts
@@ -3,8 +3,8 @@
 import { generatePageUrl } from "./utils";
 
 export const BASE_URL = "/";
-export const PRODUCTS_PER_PAGE = 6;
-export const SUPPORT_EMAIL = "support@example.com";
+export const PRODUCTS_PER_PAGE = 12;
+export const SUPPORT_EMAIL = "luka.abeywickrama@hotmail.co.uk";
 export const PROVIDERS = {
   BRAINTREE: {
     label: "Braintree",
@@ -42,23 +42,13 @@ export const STATIC_PAGES = [
 export const SOCIAL_MEDIA = [
   {
     ariaLabel: "facebook",
-    href: "https://www.facebook.com/mirumeelabs/",
+    href: "https://www.facebook.com/RuthPackhamArtist/",
     path: require("../images/facebook-icon.svg"),
   },
   {
     ariaLabel: "instagram",
-    href: "https://www.instagram.com/mirumeelabs/",
+    href: "https://www.instagram.com/ruthelizabethpackham/",
     path: require("../images/instagram-icon.svg"),
-  },
-  {
-    ariaLabel: "twitter",
-    href: "https://twitter.com/getsaleor",
-    path: require("../images/twitter-icon.svg"),
-  },
-  {
-    ariaLabel: "youtube",
-    href: "https://www.youtube.com/channel/UCg_ptb-U75e7BprLCGS4s1g/videos",
-    path: require("../images/youtube-icon.svg"),
   },
 ];
 export const META_DEFAULTS = {
